13f9ed8aa9ae7b1a0fe52517503d8bd0932489a3
Luis-Fernando-Hernandez-Reyes <116451159+luis28fer-hr@users.noreply.github.com>
2023-02-12 14:50:10 -0600

    Vista de Dashboard

---
+++ resources/views/Administrador/Modales/Perfil.blade.php
@@ -0,0 +1,40 @@
+<!-- Modal para editar perfil -->
+<div id="myModal" class="modal">
+
+    <!-- Modal content -->
+    <div class="modal-content">
+        <div class="modal-header">
+            <h2>Actualizar mi perfil</h2>
+            <i class="fa-solid fa-xmark close"></i>
+        </div>
+        <div class="modal-body">
+            <div>
+                <p><span>*</span> Nombre:</p>
+                <input type="text" placeholder="Nombre">
+                <span class="error">No debe estar vacio</span>
+            </div>
+            <div>
+                <p><span>*</span> Apellido Materno:</p>
+                <input type="text" placeholder="Nombre">
+                <span class="error">No debe estar vacio</span>
+            </div>
+            <div>
+                <p><span>*</span> Apellido Paterno:</p>
+                <input type="text" placeholder="Nombre">
+                <span class="error">No debe estar vacio</span>
+            </div>
+            <div>
+                <p><span>*</span> Foto de perfil:</p>
+                <input type="file" placeholder="Nombre">
+                <span class="error">No debe estar vacio</span>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <a href="" class="_cancel close">Cancelar</a>
+            <span>&nbsp&nbsp</span>
+            <a href="" class="_save">Guardar</a>
+        </div>
+    </div>
+
+</div>
+<!-- Fin de modal para editar perfil -->
---
+++ resources/views/Administrador/Modales/Salir.blade.php
@@ -0,0 +1,22 @@
+                        <!-- Modal para cerrar sesion -->
+                        <div id="myModal_logout" class="modal">
+
+                            <!-- Modal content -->
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h2>¿Estas seguro de cerrar sesion?</h2>
+                                    <i class="fa-solid fa-xmark close_logout"></i>
+                                </div>
+                                <div class="modal-body">
+                                    <div>
+                                        <p>Para volver a iniciar sesion necesita <span>usuario</span> y <span>contraseña
+                                            </span></p>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <a href="" class="_salir">Salir</a>
+                                </div>
+                            </div>
+
+                        </div>
+                        <!-- Fin de modal para cerrar sesion -->
