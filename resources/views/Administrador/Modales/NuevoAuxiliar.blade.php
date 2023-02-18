0478debb70b81e197ad10c7ca16a95815c345adc
Luis-Fernando-Hernandez-Reyes <116451159+luis28fer-hr@users.noreply.github.com>
2023-02-17 21:47:11 -0600

    Estilo correguido

--- resources/views/Administrador/Modales/NuevoAuxiliar.blade.php
+++ resources/views/Administrador/Modales/NuevoAuxiliar.blade.php
@@ -1,55 +1,55 @@
-<!-- Modal para añadir un nuevo auxiliar -->
-<div id="myModal_NewAux" class="modal">
+    <!-- Modal para añadir un nuevo auxiliar -->
+    <div id="myModal_NewAux" class="modal">
 
-    <!-- Modal content -->
-    <div class="modal-content">
-        <div class="modal-header">
-            <h2>Nuevo Auxiliar</h2>
-            <i class="fa-solid fa-xmark close_NewAux"></i>
+        <!-- Modal content -->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h2>Nuevo Auxiliar</h2>
+                <i class="fa-solid fa-xmark close_NewAux"></i>
+            </div>
+            <div class="modal-body">
+                <div>
+                    <p><span>*</span> Nombre:</p>
+                    <input type="text" placeholder="Nombre">
+                    <span class="error">No debe estar vacio</span>
+                </div>
+                <div>
+                    <p><span>*</span> Apellido Paterno:</p>
+                    <input type="text" placeholder="Apellido Paterno">
+                    <span class="error">No debe estar vacio</span>
+                </div>
+                <div>
+                    <p><span>*</span> Apellido Materno:</p>
+                    <input type="text" placeholder="Apellido Materno">
+                    <span class="error">No debe estar vacio</span>
+                </div>
+                <div>
+                    <p><span>*</span> Foto de perfil:</p>
+                    <input type="file" placeholder="Nombre">
+                    <span class="error">Se debe agregar un archivo</span>
+                </div>
+                <div>
+                    <p><span>*</span> Celular:</p>
+                    <input type="text" placeholder="Celular">
+                    <span class="error">No debe estar vacio</span>
+                </div>
+                <div>
+                    <p><span>*</span> Correo Electronico:</p>
+                    <input type="email" placeholder="Email">
+                    <span class="error">No debe estar vacio</span>
+                </div>
+                <div>
+                    <p><span>*</span> Contraseña:</p>
+                    <input type="password" placeholder="Contraseña">
+                    <span class="error">No debe estar vacio</span>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <a href="" class="_cancel close_NewAux">Cancelar</a>
+                <span>&nbsp&nbsp</span>
+                <a href="" class="_save">Añadir</a>
+            </div>
         </div>
-        <div class="modal-body">
-            <div>
-                <p><span>*</span> Nombre:</p>
-                <input type="text" placeholder="Nombre">
-                <span class="error">No debe estar vacio</span>
-            </div>
-            <div>
-                <p><span>*</span> Apellido Paterno:</p>
-                <input type="text" placeholder="Apellido Paterno">
-                <span class="error">No debe estar vacio</span>
-            </div>
-            <div>
-                <p><span>*</span> Apellido Materno:</p>
-                <input type="text" placeholder="Apellido Materno">
-                <span class="error">No debe estar vacio</span>
-            </div>
-            <div>
-                <p><span>*</span> Foto de perfil:</p>
-                <input type="file" placeholder="Nombre">
-                <span class="error">Se debe agregar un archivo</span>
-            </div>
-            <div>
-                <p><span>*</span> Celular:</p>
-                <input type="text" placeholder="Celular">
-                <span class="error">No debe estar vacio</span>
-            </div>
-            <div>
-                <p><span>*</span> Correo Electronico:</p>
-                <input type="email" placeholder="Email">
-                <span class="error">No debe estar vacio</span>
-            </div>
-            <div>
-                <p><span>*</span> Contraseña:</p>
-                <input type="password" placeholder="Contraseña">
-                <span class="error">No debe estar vacio</span>
-            </div>
-        </div>
-        <div class="modal-footer">
-            <a href="" class="_cancel close_NewAux">Cancelar</a>
-            <span>&nbsp&nbsp</span>
-            <a href="" class="_save">Añadir</a>
-        </div>
+
     </div>
-
-</div>
-<!-- Fin de modal para añadir un nuevo auxiliar -->
+    <!-- Fin de modal para añadir un nuevo auxiliar -->
