60e495a286ded78047ed0359028b630080c63ea1
RICARDO COLIN MALDONADO <116596667+rikicolin9@users.noreply.github.com>
2023-02-20 22:21:39 -0600

    Se agregaron las validaciones para crear un nuevo auxiliar, ademas de agregar el archivo para que este en español

--- app/Http/Controllers/auxiliaresController.php
+++ app/Http/Controllers/auxiliaresController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\auxiliares;
 use Illuminate\Http\Request;
 
 class auxiliaresController extends Controller
@@ -11,6 +12,11 @@ class auxiliaresController extends Controller
     {
 
         return view('Administrador/Auxiliares');
+    }
+
+    public function newAuxiliares(auxiliares $request)
+    {
+        return redirect('Auxiliares')->with('Nuevo_auxiliar','perfil');
     }
 
 
--- resources/views/Administrador/Modales/NuevoAuxiliar.blade.php
+++ resources/views/Administrador/Modales/NuevoAuxiliar.blade.php
@@ -1,3 +1,6 @@
+<form action="{{route('newauxiliares')}}" method="POST" enctype="multipart/form-data">
+    @csrf 
+    
     <!-- Modal para añadir un nuevo auxiliar -->
     <div id="myModal_NewAux" class="modal">
 
@@ -10,46 +13,48 @@
             <div class="modal-body">
                 <div>
                     <p><span>*</span> Nombre:</p>
-                    <input type="text" placeholder="Nombre">
-                    <span class="error">No debe estar vacio</span>
+                    <input type="text" name="name" placeholder="Ej: Arturo" value="{{old('name')}}">
+                    <span class="error">{{$errors->first('name')}}</span>
                 </div>
                 <div>
                     <p><span>*</span> Apellido Paterno:</p>
-                    <input type="text" placeholder="Apellido Paterno">
-                    <span class="error">No debe estar vacio</span>
+                    <input type="text" name="apellido_p" placeholder="Ej: Duran" value="{{old('apellido_p')}}">
+                    <span class="error">{{$errors->first('apellido_p')}}</span>
+
                 </div>
                 <div>
                     <p><span>*</span> Apellido Materno:</p>
-                    <input type="text" placeholder="Apellido Materno">
-                    <span class="error">No debe estar vacio</span>
+                    <input type="text" name="apellido_m" placeholder="Ej: Hernandez" value="{{old('apellido_m')}}">
+                    <span class="error">{{$errors->first('apellido_m')}}</span>
                 </div>
                 <div>
                     <p><span>*</span> Foto de perfil:</p>
-                    <input type="file" placeholder="Nombre">
-                    <span class="error">Se debe agregar un archivo</span>
+                    <input type="file" name="fotoPerfil" accept="image/*">
+                    <span class="error">{{$errors->first('fotoPerfil')}}</span>
                 </div>
                 <div>
                     <p><span>*</span> Celular:</p>
-                    <input type="text" placeholder="Celular">
-                    <span class="error">No debe estar vacio</span>
+                    <input type="text" name="numCel" placeholder="ej: 448123649" value="{{old('numCel')}}">
+                    <span class="error">{{$errors->first('numCel')}}</span>
                 </div>
                 <div>
                     <p><span>*</span> Correo Electronico:</p>
-                    <input type="email" placeholder="Email">
-                    <span class="error">No debe estar vacio</span>
+                    <input type="text" name="email" placeholder="aaaa@gmail.com" value="{{old('email')}}">
+                    <span class="error">{{$errors->first('email')}}</span>
                 </div>
                 <div>
                     <p><span>*</span> Contraseña:</p>
-                    <input type="password" placeholder="Contraseña">
-                    <span class="error">No debe estar vacio</span>
+                    <input type="text" name="pass" placeholder="********" value="{{old('pass')}}">
+                    <span class="error">{{$errors->first('pass')}}</span>
                 </div>
             </div>
             <div class="modal-footer">
                 <a href="" class="_cancel close_NewAux">Cancelar</a>
                 <span>&nbsp&nbsp</span>
-                <a href="" class="_save">Añadir</a>
+                <button type="submit" class="_save">Añadir</button>
             </div>
         </div>
 
     </div>
     <!-- Fin de modal para añadir un nuevo auxiliar -->
+</form>
