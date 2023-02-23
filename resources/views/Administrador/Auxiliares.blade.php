da2c5124517e661b9c197ece45f94630c1589cce
Luis-Fernando-Hernandez-Reyes <116451159+luis28fer-hr@users.noreply.github.com>
2023-02-22 22:25:23 -0600

    Modales javaScript

--- resources/views/Administrador/Auxiliares.blade.php
+++ resources/views/Administrador/Auxiliares.blade.php
@@ -4,79 +4,78 @@
 
 @section('contenido_Administrador')
 
-@if (session()->has('Nuevo_auxiliar'))
-{!!"<script> Swal.fire({
-    icon: 'success',
-    title: '¡Tus datos se han agregado!',
-    text: 'Administrador',
-    })</script> "!!}
-@endif
+    @if (session()->has('Nuevo_auxiliar'))
+        {!! "<script> Swal.fire({
+            icon: 'success',
+            title: '¡Tus datos se han agregado!',
+            text: 'Administrador',
+            })</script> " !!}
+    @endif
 
-@if (session()->has('Editar_auxiliar'))
-{!!"<script> Swal.fire({
-    icon: 'success',
-    title: '¡Tus datos se han actualizado!',
-    text: 'Administrador',
-    })</script> "!!}
-@endif
+    @if (session()->has('Editar_auxiliar'))
+        {!! "<script> Swal.fire({
+            icon: 'success',
+            title: '¡Tus datos se han actualizado!',
+            text: 'Administrador',
+            })</script> " !!}
+    @endif
 
-<main>
-    <h1>Auxiliares</h1>
-    <div class="auxiliares-options">
-        <a id="myBtn_NewAux"><i class="fa-regular fa-square-plus"> </i> Nuevo</a>
-        @include('Administrador/Modales/NuevoAuxiliar')
-        <div class="filtros">
-            <div>
-                <select name="" id="">
-                    <option value="" selected>Carga de trabajo</option>
-                    <option value="">10%</option>
-                </select>
+    <main>
+        <h1>Auxiliares</h1>
+        <div class="auxiliares-options">
+            <a id="myBtn_NewAux"><i class="fa-regular fa-square-plus"> </i> Nuevo</a>
+            @include('Administrador/Modales/NuevoAuxiliar')
+            <div class="filtros">
+                <div>
+                    <select name="" id="">
+                        <option value="" selected>Carga de trabajo</option>
+                        <option value="">10%</option>
+                    </select>
 
-            </div>
-           <div>
-            <input type="text" placeholder="Buscar por Nombre"><i class="fa-solid fa-magnifying-glass"></i>
-           </div>
-        </div>
-    </div>
-    <hr>
-
-    <section class="auxiliares">
-
-        <div class="auxiliares-container">
-            @foreach($consulAuxiliares as $consul)
-            <div class="auxiliares-card">
-                <div class="img-container">
-                    <img src="{{$consul->url_foto}}" alt="FotodePerfil">
                 </div>
-                <div class="auxiliares-name">
-                    <p>{{$consul->name}} {{$consul->apellido_p}} {{$consul->apellido_m}}</p>
-                </div>
-                <div  class="auxiliares-data">
-                    <p>ID: {{$consul->id}}</p>
-                    <p>{{$consul->email}}</p>
-                    <p>Tel: {{$consul->num_telefono}}</p>
-                    <p>--------------------</p>
-                </div>
-                <div class="auxiliares-bar">
-                    <p class="">60 %</p>
-                </div>
-                <div class="auxiliares-btns">
-                    <a id="myBtn_EditAux" class="btn-edit"><i class="fa-solid fa-pen"></i></a>
-
-                    @include('Administrador/Modales/EditarAuxiliar')
-
-                    <a id="myBtn_DeleteAux"class="btn-delete"><i class="fa-solid fa-trash-can"></i></a>
-                    @include('Administrador/Modales/EliminarAuxiliar')
+                <div>
+                    <input type="text" placeholder="Buscar por Nombre"><i class="fa-solid fa-magnifying-glass"></i>
                 </div>
             </div>
-            @endforeach
         </div>
-    </section>
+        <hr>
+
+        <section class="auxiliares">
+
+            <div class="auxiliares-container">
+                @foreach ($consulAuxiliares as $consul)
+                    <div class="auxiliares-card">
+                        <div class="img-container">
+                            <img src="{{ $consul->url_foto }}" alt="FotodePerfil">
+                        </div>
+                        <div class="auxiliares-name">
+                            <p>{{ $consul->name }} {{ $consul->apellido_p }} {{ $consul->apellido_m }}</p>
+                        </div>
+                        <div class="auxiliares-data">
+                            <p>ID: {{ $consul->id }}</p>
+                            <p>{{ $consul->email }}</p>
+                            <p>Tel: {{ $consul->num_telefono }}</p>
+                            <p>--------------------</p>
+                        </div>
+                        <div class="auxiliares-bar">
+                            <p class="">60 %</p>
+                        </div>
+                        <div class="auxiliares-btns">
+                            <a onclick="modalEditarMostrar({{ $consul->id }})" class="btn-edit"><i class="fa-solid fa-pen"></i></a>
+
+                            @include('Administrador/Modales/EditarAuxiliar')
+
+                            <a onclick="modalEliminarMostrar({{ $consul->id }})" class="btn-delete"><i class="fa-solid fa-trash-can"></i></a>
+                            @include('Administrador/Modales/EliminarAuxiliar')
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </section>
 
 
-</main>
+    </main>
 
 
 
 @endsection
-
