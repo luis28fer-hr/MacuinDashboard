64228b374688a85c40e58a99ab9f5c76e612655c
RICARDO COLIN MALDONADO <116596667+rikicolin9@users.noreply.github.com>
2023-02-23 20:09:00 -0600

    Se agrego el codigo para insertar registros de un nuevo departamento

--- app/Http/Controllers/departamentosController.php
+++ app/Http/Controllers/departamentosController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Requests\departamentos;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Carbon;
 
 class departamentosController extends Controller
 {
@@ -16,6 +18,18 @@ class departamentosController extends Controller
     }
 
     public function newDepartamento(departamentos $request ){
+
+        $img =$request->file('fotoPerfil')->store('public/img');
+        $url = Storage::url($img);
+
+            DB::table('departamentos')->insert([
+                "nombre"=>$request->input('name'),
+                "descripcion"=>$request->input('descripcion'),
+                "url_foto"=> $url,
+                "created_at"=>Carbon::now(),
+                "updated_at"=>Carbon::now()
+            ]);
+
         return redirect('departamentos')->with('Nuevo_departamento','departamentos');
     }
     public function editDepartamento(departamentos $request ){
