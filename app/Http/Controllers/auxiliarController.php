862a31fe3c1646383806f9e6d49d1611183a2338
RICARDO COLIN MALDONADO <116596667+rikicolin9@users.noreply.github.com>
2023-03-13 23:21:32 -0600

    Generar reportes PDF de los tickets del perfil de auxiliar

--- app/Http/Controllers/auxiliarController.php
+++ app/Http/Controllers/auxiliarController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Carbon;
 use App\Http\Requests\searchTickets;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class auxiliarController extends Controller
 {
@@ -22,7 +23,7 @@ class auxiliarController extends Controller
 
         // Consulta de Departamentos
         $consulDepartaments = DB::table('departamentos')->get();
-        
+
         return view('Auxiliar/Tickets', compact('consultaTickets', 'consulDepartaments'));
     }
 
@@ -78,7 +79,7 @@ class auxiliarController extends Controller
 
     public function nuevoMensaje(comentario $request, $id_ticket)
     {
-        
+
         if ($request->input('comentario') == null) {
 
             return redirect('auxiliar/tickets')->with('MensajeNoEnviado', 'Ticket');
@@ -88,7 +89,7 @@ class auxiliarController extends Controller
                 "comentario" =>  $request->input('comentario'),
                 "created_at" => Carbon::now(),
             ]);
-            
+
             return redirect('auxiliar/tickets')->with('MensajeEnviado', 'Ticket');
         }
     }
@@ -121,7 +122,7 @@ class auxiliarController extends Controller
         } elseif (empty($estatus) and empty($fecha)) { //Busqueda por Departamento
 
             $consultaTickets = DB::select('select * from tickets as t, auxiliares as a
-              where (t.auxiliar_id = a.id_auxiliar and usuario_id = ?) and t.cliente_id in(select c.id_cliente from clientes as c where c.departamento_id = ?)', [Auth::user()->id,$departamento]);
+              where (t.auxiliar_id = a.id_auxiliar and usuario_id = ?) and t.cliente_id in(select c.id_cliente from clientes as c where c.departamento_id = ?)', [Auth::user()->id, $departamento]);
 
             if ($consultaTickets != null) {
                 $consultaTickets =  $this->asignarDatos($consultaTickets);
@@ -134,7 +135,7 @@ class auxiliarController extends Controller
 
 
             $consultaTickets = DB::select('select * from tickets as t, auxiliares as a where (t.auxiliar_id = a.id_auxiliar
-            and usuario_id = ?) and t.estatus = ?', [Auth::user()->id,$estatus]);
+            and usuario_id = ?) and t.estatus = ?', [Auth::user()->id, $estatus]);
 
 
 
@@ -159,7 +160,7 @@ class auxiliarController extends Controller
         } elseif (empty($departamento)) { //Busqueda por Estatus y Fecha
 
             $consultaTickets = DB::select('select * from tickets as t, auxiliares as a where (t.auxiliar_id = a.id_auxiliar
-            and usuario_id = ?) and date(t.created_at) = ? and t.estatus=?', [Auth::user()->id,$fecha,$estatus]);
+            and usuario_id = ?) and date(t.created_at) = ? and t.estatus=?', [Auth::user()->id, $fecha, $estatus]);
 
 
             if ($consultaTickets != null) {
@@ -191,7 +192,6 @@ class auxiliarController extends Controller
 
                 $consultaTickets =  $this->asignarDatosFiltro($consultaTickets);
                 return view('Auxiliar/Tickets', compact('consultaTickets', 'consulDepartaments'));
-
             } else {
                 return redirect('auxiliar/tickets')->with('noExiste', 'Ticket');
             }
@@ -219,5 +219,154 @@ class auxiliarController extends Controller
         }
 
         return $consultaTickets;
+    }
+
+    public function generatePDF($id)
+    {
+        $consultaTicket = DB::table('tickets')->where('id_ticket', $id)->first();
+        $consultaTicket->auxiliar = DB::table('auxiliares')->select(['usuario_id'])->where('id_auxiliar', $consultaTicket->auxiliar_id)->first();
+        $consultaTicket->auxiliar->datos = DB::table('users')->select(['name', 'apellido_p', 'apellido_m'])->where('id', $consultaTicket->auxiliar->usuario_id)->first();
+        $consultaTicket->cliente = DB::table('clientes')->select(['usuario_id', 'departamento_id'])->where('id_cliente', $consultaTicket->cliente_id)->first();
+        $consultaTicket->cliente->datos = DB::table('users')->select(['name', 'apellido_p', 'apellido_m'])->where('id',  $consultaTicket->cliente->usuario_id)->first();
+        $consultaTicket->cliente->departamento = DB::table('departamentos')->select(['nombre'])->where('id_departamento',  $consultaTicket->cliente->departamento_id)->first();
+        $consultaTicket->comentarioAdminAux = DB::select('select * from cometarioadministrador where ticket_id = ? and tipo = 1 ORDER BY created_at DESC', [$consultaTicket->id_ticket]);
+        $consultaTicket->comentarioAuxCli = DB::select('select * from comentarioauxiliar where ticket_id = ? ORDER BY created_at DESC', [$consultaTicket->id_ticket]);
+
+        $pdf = PDF::loadView('Auxiliar/Reportes/Ticket', compact('consultaTicket'));
+        return $pdf->stream();
+    }
+
+    public function generatePDFfiltro(searchTickets $request)
+    {
+
+        $estatus = $request->input('auxiliar');
+        $departamento = $request->input('searchByDepartamento');
+        $fecha = $request->input('searchByFecha');
+
+
+
+
+        if ($estatus != 0 and $departamento == 0 and $fecha == null) { //Verifica que estatus tenga valor diferente a todos
+
+            $consultaTickets = DB::select('select * from tickets as t, auxiliares as a where (t.auxiliar_id = a.id_auxiliar
+            and usuario_id = ?) and t.estatus = ?', [Auth::user()->id, $estatus]);
+
+            $consultaTickets =  $this->asignarDatos($consultaTickets);
+
+            if ($consultaTickets != null) {
+                $pdf = PDF::loadView('Auxiliar/Reportes/Estatus', compact('consultaTickets'));
+                return $pdf->stream();
+            } else {
+                return redirect('auxiliar/tickets')->with('sinRegistros', 'Ticket');
+            }
+        }
+
+        if ($estatus != 0 and $departamento != 0 and $fecha == null) { //Verifica que estatus y departamento tengan un valor diferente a todos
+
+            $consultaTickets = DB::select('select * from tickets as t, auxiliares as a where (t.auxiliar_id = a.id_auxiliar
+            and usuario_id = ?) and date(t.created_at) = ? and t.estatus=?', [Auth::user()->id, $fecha, $estatus]);
+
+            $consultaTickets =  $this->asignarDatos($consultaTickets);
+
+            if ($consultaTickets != null) {
+                $pdf = PDF::loadView('Auxiliar/Reportes/EstatusDepartamento', compact('consultaTickets'));
+                return $pdf->stream();
+            } else {
+                return redirect('auxiliar/tickets')->with('sinRegistros', 'Ticket');
+            }
+        }
+
+
+        if ($estatus != 0 and $departamento != 0 and $fecha != null) { //Verifica que estatus, departamento y fecha tenga valor difernte a todos y null
+
+            $consultaTickets = DB::select('select * from tickets as t, auxiliares as a
+            where (t.auxiliar_id = a.id_auxiliar and usuario_id = ?) and t.cliente_id in(select c.id_cliente from clientes as c
+            where c.departamento_id = ?) and date(t.created_at) = ? and t.estatus =?', [Auth::user()->id, $departamento, $fecha, $estatus]);
+
+            $consultaTickets =  $this->asignarDatos($consultaTickets);
+
+            if ($consultaTickets != null) {
+                $pdf = PDF::loadView('Auxiliar/Reportes/General', compact('consultaTickets'));
+                return $pdf->stream();
+            } else {
+                return redirect('auxiliar/tickets')->with('sinRegistros', 'Ticket');
+            }
+        }
+
+        if ($estatus == 0 and $departamento != 0 and $fecha == null) { //Verifica que departamento sea el unico en pdf
+
+            $consultaTickets = DB::select('select * from tickets as t, auxiliares as a
+            where (t.auxiliar_id = a.id_auxiliar and usuario_id = ?) and t.cliente_id in(select c.id_cliente from clientes as c where c.departamento_id = ?)', [Auth::user()->id, $departamento]);
+
+            $consultaTickets =  $this->asignarDatos($consultaTickets);
+
+            if ($consultaTickets != null) {
+                $pdf = PDF::loadView('Auxiliar/Reportes/Departamento', compact('consultaTickets'));
+                return $pdf->stream();
+            } else {
+                return redirect('auxiliar/tickets')->with('sinRegistros', 'Ticket');
+            }
+        }
+
+        if ($estatus == 0 and $departamento != 0 and $fecha != null) { //Verifica que departamento y fecha
+
+            $consultaTickets = DB::select('select * from tickets as t, auxiliares as a
+            where (t.auxiliar_id = a.id_auxiliar and usuario_id = ?) and t.cliente_id in(select c.id_cliente from clientes as c
+            where c.departamento_id = ?) and date(t.created_at) = ?', [Auth::user()->id, $departamento, $fecha]);
+
+            $consultaTickets =  $this->asignarDatos($consultaTickets);
+
+            if ($consultaTickets != null) {
+                $pdf = PDF::loadView('Auxiliar/Reportes/DepartamentoFecha', compact('consultaTickets'));
+                return $pdf->stream();
+            } else {
+                return redirect('auxiliar/tickets')->with('sinRegistros', 'Ticket');
+            }
+        }
+
+        if ($estatus == 0 and $departamento == 0 and $fecha != null) { //Verifica fecha
+
+            $consultaTickets = DB::select('select * from tickets as t, auxiliares as a where (t.auxiliar_id = a.id_auxiliar
+            and usuario_id = ?) and date(t.created_at) = ?', [Auth::user()->id, $fecha]);
+
+            $consultaTickets =  $this->asignarDatos($consultaTickets);
+
+            if ($consultaTickets != null) {
+                $pdf = PDF::loadView('Auxiliar/Reportes/Fecha', compact('consultaTickets'));
+                return $pdf->stream();
+            } else {
+                return redirect('auxiliar/tickets')->with('sinRegistros', 'Ticket');
+            }
+        }
+
+        if ($estatus != 0 and $departamento == 0 and $fecha != null) { //Verifica fecha y estatus
+
+            $consultaTickets = DB::select('select * from tickets as t, auxiliares as a where (t.auxiliar_id = a.id_auxiliar
+            and usuario_id = ?) and date(t.created_at) = ? and t.estatus=?', [Auth::user()->id, $fecha, $estatus]);
+
+            $consultaTickets =  $this->asignarDatos($consultaTickets);
+
+            if ($consultaTickets != null) {
+                $pdf = PDF::loadView('Auxiliar/Reportes/FechaEstatus', compact('consultaTickets'));
+                return $pdf->stream();
+            } else {
+                return redirect('auxiliar/tickets')->with('sinRegistros', 'Ticket');
+            }
+        }
+
+
+
+        if ($estatus == 0 and $departamento == 0 and $fecha == null) { //Verifica  sean todos, departamento todos  y la fecha sea cualquiera
+
+            $consultaTickets = DB::select('select * from tickets as t, auxiliares as a where t.auxiliar_id = a.id_auxiliar and usuario_id = ?', [Auth::user()->id]);
+            $consultaTickets =  $this->asignarDatos($consultaTickets);
+
+            if ($consultaTickets != null) {
+                $pdf = PDF::loadView('Auxiliar/Reportes/General', compact('consultaTickets'));
+                return $pdf->stream();
+            } else {
+                return redirect('auxiliar/tickets')->with('sinRegistros', 'Ticket');
+            }
+        }
     }
 }
